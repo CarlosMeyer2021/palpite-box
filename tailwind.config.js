a8474873e26f413be0861e1212b04b07781b1305
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2021-05-20 22:26:01 -0300

    Carlos
    Tela Pesquisa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
   },
   variants: {
     extend: {},
